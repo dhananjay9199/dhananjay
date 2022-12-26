97ba1e8d49062a3d1e3cc31d9fad20f459800d11
dhananjay <dchaudhari758@gmail.com>
2022-12-26 18:53:20 +0530

    modified file1.java

--- file1.java
+++ file1.java
@@ -1,5 +1,9 @@
 public class file{
 	public static void main(){
-	System.out.println("dhananjay chaudhari");
+	int x=5;
+	int y=10;
+	System.out.println("x :"+x);
+	System.out.println("y :"+y);
+	System.out.println("hello wrold");
 }
 }
