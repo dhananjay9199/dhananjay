2ae72dc69d2cb370c3450a33997e5300fd36d545
dhananjay <dchaudhari758@gmail.com>
2022-12-26 17:33:48 +0530

    initial commit

---
+++ file.java
@@ -0,0 +1,9 @@
+public class file{
+	public static void main(){
+	int x=5;
+	int y=10;
+	System.out.println("x :"+x);
+	System.out.println("y :"+y);
+	System.out.println("hello wrold");
+}
+}
---
+++ file1.java
@@ -0,0 +1,5 @@
+public class file{
+	public static void main(){
+	System.out.println("dhananjay chaudhari");
+}
+}
